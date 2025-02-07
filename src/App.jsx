07c2816f4c230f2770fe187ed2d8854afde1be8d
Ampomaah24 <165961822+Ampomaah24@users.noreply.github.com>
2025-02-07 21:18:12 +0000

    Initial Conference Room Listings Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import Homepage from "./pages/Homepage"; // Import Homepage
 import ServicesPage from "./pages/ServicesPage"; // Import Services Page
 import SignUp from "./pages/SignUp"; // Import Sign Up Page
 
+import CListings from "./pages/CListings"; // Import Conference Listings Page
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} /> {/* Homepage as default route */}
-        <Route path="/services" element={<ServicesPage />} /> {/* Services Page route */}
-        <Route path="/signup" element={<SignUp />} /> {/* Sign Up Page route */}
+        <Route path="/services" element={<ServicesPage />} /> {/* Services Page */}
+        <Route path="/signup" element={<SignUp />} /> {/* Sign Up Page */}
+     
+        <Route path="/conference-listings" element={<CListings />} /> {/* Conference Listings Page */}
       </Routes>
     </Router>
   );
