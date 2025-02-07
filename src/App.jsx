2389b71c604992494fa43e26c162b24988cca3b9
Ampomaah24 <165961822+Ampomaah24@users.noreply.github.com>
2025-02-07 01:35:47 +0000

    Initial Services Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage"; // Import Homepage
+import ServicesPage from "./pages/ServicesPage"; // Import Services Page
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} /> {/* Homepage as default route */}
+        <Route path="/services" element={<ServicesPage />} /> {/* Services Page route */}
       </Routes>
     </Router>
   );
